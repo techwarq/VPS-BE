85a1c5b14bdbd36d89d962aa1179c90556238cbf
Sonali Nayak <sonalinayak@Sonalis-MacBook-Air.local>
2025-10-02 15:40:40 +0530

    made changes added signed urls

--- src/index.ts
+++ src/index.ts
@@ -272,7 +272,7 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction): void => {
   console.error('Error:', err);
   res.status(500).json({
     error: 'Internal server error',
-    message: process.env.NODE_ENV === 'development' ? err.message : 'Something went wrong'
+    message: process.env.NODE_ENV === 'development' ? err.message : 'Something went REALLY WRONG'
   });
 });
 
